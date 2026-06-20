c0aba7b86a2bb5b9b5ef61bd37b812f16db225a2
chanduzumba <chanduzumba@gmail.com>
2026-06-21 01:21:25 +0530

    get product by id route and controller logic added

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,12 +1,42 @@
-import Product from "../models/Product.models.js" //import product model
+import Product from "../models/Product.models.js"; //import product model
 
 //get allproducts controller which requests / queries mongodb for reading all data
 export const getAllProductsController = async (req, res) => {
-    //logic to get all data from DB
-    try {
-        const products = await Product.find() // query to get all data
-        return res.status(200).json(products) //set response status as success and send json response
-    } catch (err) {
-        return res.status(500).json({message: err.message}) //send error response
+  //logic to get all data from DB
+  try {
+    const products = await Product.find(); // query to get all data
+    return res.status(200).json(products); //set response status as success and send json response
+  } catch (err) {
+    return res.status(500).json({ message: err.message }); //send error response
+  }
+};
+
+//get a single product by id
+export const getProductByIdController = async (req, res) => {
+  //logic to get a product
+  try {
+    const productId = req.params.id
+    if (isNaN(productId)) {
+      //check if not a number
+      //return bad request as response
+      return res.status(400).json({
+        message: "Invalid product ID",
+      });
     }
-}
+    // query DB to find one with param id
+    const product = await Product.findOne({ id: productId });
+
+    //if product not present return not found message
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+    //if found return product details
+    return res.status(200).json({
+      product,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err.message }); //send error response
+  }
+};
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,9 +1,10 @@
-import { getAllProductsController } from "../controllers/products.controller.js"
+import { getAllProductsController, getProductByIdController } from "../controllers/products.controller.js"
 
 //separate routes section for products
 const productRoutes = (app) => {
     //use express app/server for routing http requests and handle logic in controller
     app.get('/products', getAllProductsController)
+    app.get('/products/:id', getProductByIdController)
 }
 
 export default productRoutes
